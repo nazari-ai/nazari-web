aed6e0d748b2b5fecbfa435f952a9b00b53a801b
oladetounjed <oladetountemitayo@gmail.com>
2022-05-26 00:12:26 +0100

    Improve Dashboard

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,13 +1,22 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
-import MemoHome from "src/components/Icons/HomeIcon";
+import Link from "next/link";
 import { DashboardLayout } from "src/layouts/DashboardLayout";
+import styles from "../../styles/dashboard.module.scss";
 
 const Home: NextPage = () => {
     return (
         <DashboardLayout>
-            <div></div>
+            <div className={styles.dashboardContainer}>
+                <div className={styles.dashboardHeader}>
+                    <h1 className={styles.dashboardGreeting}>Good Evening!</h1>
+                    <p className={styles.dashboardNote}>
+                        Get an idea of how Algorand is done on <Link href="/dashboard/twitter">Twitter</Link>,{" "}
+                        <Link href="/dashboard/github">Github</Link> and <Link href="/dashboard/reddit">Reddit.</Link>
+                    </p>
+                </div>
+            </div>
         </DashboardLayout>
     );
 };
