52cb09285defb5a0c75a4f743c8d62c9f2411939
oladetounjed <oladetountemitayo@gmail.com>
2022-05-29 13:38:07 +0100

    Create Dashboard Summary Line Chart and Complete Home layout

---
+++ src/components/SummaryLineChart/index.tsx
@@ -0,0 +1,58 @@
+import Image from "next/image";
+import React from "react";
+import styles from "./style.module.scss";
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, ResponsiveContainer } from "recharts";
+
+type Props = {
+    title?: any;
+};
+
+export function SummaryLineChart(props: Props) {
+    const data = [
+        {
+            name: "Sun",
+            data: 3490,
+        },
+        {
+            name: "Mon",
+            data: 4000,
+        },
+        {
+            name: "Tues",
+            data: 3000,
+        },
+        {
+            name: "Wed",
+            data: 2000,
+        },
+        {
+            name: "Thurs",
+            data: 0,
+        },
+        {
+            name: "Fri",
+            data: 1890,
+        },
+        {
+            name: "Sat",
+            data: 2390,
+        },
+    ];
+
+    return (
+        <div className={styles.chartContainer}>
+            <ResponsiveContainer height={200}>
+                <LineChart data={data}>
+                    <Line type="monotone" dataKey="data" stroke="#6FD791" />
+                    <Tooltip />
+                    {/* <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis /> */}
+                </LineChart>
+            </ResponsiveContainer>
+            <div className={styles.chartHeaderContainer}>
+                <h1 className={styles.chartTitle}>{props.title}</h1>
+            </div>
+        </div>
+    );
+}
