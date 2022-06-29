63c208dad7010f870ba41b33df24a8390bf79864
oladetounjed <oladetountemitayo@gmail.com>
2022-06-29 10:50:29 +0100

    Fix naming issue

--- pages/about.tsx
+++ pages/about.tsx
@@ -4,9 +4,9 @@ import { HomePageHero } from "../src/sections/HomePageHero";
 import { useState } from "react";
 import { AboutPageHero } from "src/sections/AboutPageHero";
 import { DefaultLayout } from "src/layouts/DefaultLayout";
-import { AboutPageMission } from "src/sections/AboutPageMision";
+import { AboutPageMission } from "src/sections/AboutPageMission";
 
-const Home: NextPage = () => {
+const About: NextPage = () => {
     const [close, setClose] = useState(false);
     return (
         <DefaultLayout>
@@ -16,4 +16,4 @@ const Home: NextPage = () => {
     );
 };
 
-export default Home;
+export default About;
