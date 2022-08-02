ee328c777560b829d6141f6550d85ee6d33aaf62
oladetounjed <oladetountemitayo@gmail.com>
2022-08-02 01:01:18 +0100

    Fix Type Error and other improvements

--- src/sections/RedditSummary/index.tsx
+++ src/sections/RedditSummary/index.tsx
@@ -6,9 +6,9 @@ export function RedditSummary() {
         <div className={styles.summaryContainer}>
             <h1 className={styles.summaryHeader}>Reddit Summary</h1>
             <div className={styles.chartContainer}>
-                <SummaryLineChart title="Comments" />
+                {/* <SummaryLineChart title="Comments" />
                 <SummaryLineChart title="Upvotes" />
-                <SummaryLineChart title="Posts" />
+                <SummaryLineChart title="Posts" /> */}
             </div>
         </div>
     );
